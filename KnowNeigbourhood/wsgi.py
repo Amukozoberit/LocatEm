5c2805233a091703b4a35da282ee9b194d791337
Amukozoberit <mwasheb@gmail.com>
2021-09-26 10:21:34 +0300

    others

---
+++ KnowNeigbourhood/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for KnowNeigbourhood project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KnowNeigbourhood.settings')
+
+application = get_wsgi_application()
---
+++ neigbouhood/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import home, prof
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    path('',views.home, name='index'),
+    
+    path('biz/<str:id>/',views.biz,name='biz'),
+    path('profile',views.update_profile,name='profile'),
+    path('prof/<str:id>/',views.prof,name='prof'),
+    path('search',views.search_results,name='search'),
+    path('createPost',views.create_post,name='createPost'),
+    path('news', views.news,name='news'),
+    path('createHood',views.create_neiba,name='createHood'),
+    path('delHood/<str:name>',views.delHood,name='delHood'),
+    path('neibahood',views.neibas,name='neibaHood'),
+    path('users',views.users,name='users'),
+    path('updateHood/<str:id>',views.update_Neibas,name='updateHood'),
+    path('updateoCC/<str:id>',views.update_Occupants,name='updateoCC'),
+]
